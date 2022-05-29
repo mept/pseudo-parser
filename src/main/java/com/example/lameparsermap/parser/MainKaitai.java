22e55e5a3acc191fe224d79cf252fc606c78538d
mept <localhost>
2022-05-30 00:27:29 +0300

    init

---
+++ src/main/java/com/example/lameparsermap/parser/MainKaitai.java
@@ -0,0 +1,26 @@
+package com.example.lameparsermap.parser;
+
+import com.example.lameparsermap.EthernetFrame;
+import com.example.lameparsermap.Ipv4Packet;
+import com.example.lameparsermap.Pcap;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class MainKaitai {
+
+    public static void main(String[] args) throws IOException {
+        Pcap pcap = Pcap.fromFile("src/main/resources/gsm_map_with_ussd_string.pcap");
+        ((Ipv4Packet) ((EthernetFrame) pcap.packets().get(0).body()).body()).body()._io().readBytes(144); // read until map
+        byte[] mapBytes = ((Ipv4Packet) ((EthernetFrame) pcap.packets().get(0).body()).body()).body()._io().readBytesFull();
+
+        byte invokeId = mapBytes[4];
+        byte opCode = mapBytes[7];
+        byte[] ussdBytes = Arrays.copyOfRange(mapBytes, 15, 29);
+        String ussdString = javax.xml.bind.DatatypeConverter.printHexBinary(ussdBytes);
+
+        System.out.println("invokeId = " + invokeId);
+        System.out.println("opCode = " + opCode);
+        System.out.println("ussdString = " + ussdString);
+    }
+}
